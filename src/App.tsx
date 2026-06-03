2ddc8c88067edcb4b9cb488ae0609a8b22c23ae8
Kauã_Henrique_Dev <kauahenriquesx09@gmail.com>
2026-06-03 12:04:03 -0300

    deploy(fix)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/global.css';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { Sobre } from './pages/Sobre';
 import { Servicos } from './pages/Servicos';
 import { Depoimentos } from './pages/Depoimentos';
-import { Footer } from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Home />
-      <Sobre />
-      <Servicos />
-      <Depoimentos />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/servicos" element={<Servicos />} />
+        <Route path="/depoimentos" element={<Depoimentos />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
